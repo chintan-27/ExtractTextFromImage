8aaa3867267d9994e49bbd7509d47f6a5d19fc00
Chintan Acharya <chintan.acharya27@gmail.com>
2021-10-25 18:14:02 +0530

    Added blur detection

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from ocr_core import ocr_core
+from blur import test_blurry
 
 UPLOAD_FOLDER = '/static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -27,37 +28,34 @@ def upload_page():
         if 'file' not in request.files:
             return jsonify(message="No file selected")
         file = request.files['file']
-
-        # if user does not select file, browser also
-        # submit a empty part without filename
         if file.filename == '':
-            return jsonify(sucess=False, message="No file selected")
+            return jsonify(success=False, message="No file selected")
 
         if file and allowed_file(file.filename):
             file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
 
             # call the OCR function on it
+            check_blur = test_blurry(file.filename)
+            if (check_blur['fm'] > 100):
+                return jsonify(
+                    success=False,
+                    message=
+                    "Image too blurred, click a better image for accurate results",
+                    blur=check_blur)
             extracted_text = ocr_core(file)
             response = jsonify(
-                sucess=True,
-                message="Successful",
+                success=True,
                 response=extracted_text,
                 link="https://extract-text-image.herokuapp.com/static/uploads/"
                 + file.filename)
             response.headers.add("Access-Control-Allow-Origin", "*")
-
-            # extract the text and display it
-            # return render_template('upload.html',
-            #                        msg='Successfully processed',
-            #                        extracted_text=extracted_text,
-            #                        img_src=UPLOAD_FOLDER + file.filename)
             return response
         else:
-            return jsonify(sucess=False,
+            return jsonify(success=False,
                            message="Only jpg, jpeg, png and gif files allowed")
 
     elif request.method == 'GET':
-        return jsonify(sucess=False, message="Cannot GET")
+        return jsonify(success=False, message="Cannot GET")
 
 
 if __name__ == '__main__':
